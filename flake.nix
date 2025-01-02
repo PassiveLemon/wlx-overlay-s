4b2ad0c4af08bb5855c573db2c07433dcf11d3c5
PassiveLemon <lemonl3mn@protonmail.com>
2025-01-02 00:18:56 -0500

    Feature: uidev flake devshell

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  description = "WlxOverlay-S-WayVR";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { ... } @ inputs:
+  inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+    systems = [ "x86_64-linux" ];
+
+    perSystem = { self', system, ... }:
+    let
+      pkgs = import inputs.nixpkgs { inherit system; };
+    in
+    {
+      # To use, just checkout the last stable release and enter the shell
+      devShells = {
+        default = pkgs.mkShell {
+          packages = [
+            self'.packages.default.nativeBuildInputs
+            self'.packages.default.buildInputs
+          ];
+        };
+      };
+      packages = {
+        default = pkgs.wlx-overlay-s.overrideAttrs (prevAttrs: {
+          pname = "wlx-overlay-s-wayvr";
+          version = "0-unstable-2024-10-18";
+
+          src = ./.;
+
+          useFetchCargoVendor = true;
+          cargoHash = "";
+
+          buildInputs = prevAttrs.buildInputs ++ (with pkgs; [
+            cmake
+            libGL
+            python3
+            wayland
+          ]);
+        });
+      };
+    };
+  };
+}
+
